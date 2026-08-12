62b63ecfc976d1a2470bbb8f872d85dca8fa5fdd
xingqianyue268-alt <xingqianyue268@gmail.com>
2026-08-12 10:59:30 +0800

    chore: expose execution config and bump package version

--- src/req2test/__init__.py
+++ src/req2test/__init__.py
@@ -1,7 +1,14 @@
 """Req2Test Agent package."""
 
 from .config import GenerationConfig, LLMSettings
+from .execution_models import ExecutionConfig, HttpTestSpec
 from .graph import run_workflow
 
-__all__ = ["GenerationConfig", "LLMSettings", "run_workflow"]
-__version__ = "0.1.0"
+__all__ = [
+    "GenerationConfig",
+    "LLMSettings",
+    "ExecutionConfig",
+    "HttpTestSpec",
+    "run_workflow",
+]
+__version__ = "0.3.0"
